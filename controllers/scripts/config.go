357d67468c46a3bfbbb85ea20a034a816cc5ad9d
Oleg <97077423+RobotSail@users.noreply.github.com>
2022-09-16 16:50:13 -0400

    Update controllers/scripts/config.go
    Co-authored-by: Hector Sanjuan <code@hector.link>

--- controllers/scripts/config.go
+++ controllers/scripts/config.go
@@ -244,8 +244,7 @@ func applyIPFSClusterK8sDefaults(conf *config.Config, storageMax string, peers [
 	conf.Swarm.RelayClient = rc
 	conf.Peering.Peers = peers
 	conf.Experimental.AcceleratedDHTClient = true
-	// make sure that we're not announcing or filtering any addresses to avoid
-	// searching entire subnets for peers
+	// make sure that we're not announcing or filtering any addresses to ensure Kubo daemons can connect via LAN
 	// issue: https://github.com/ipfs-cluster/ipfs-operator/issues/34
 	conf.Addresses.NoAnnounce = make([]string, 0)
 	conf.Swarm.AddrFilters = make([]string, 0)

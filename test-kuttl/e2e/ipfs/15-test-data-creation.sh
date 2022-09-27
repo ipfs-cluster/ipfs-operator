8d3a8e7737ca626b73b28b1aa492766e4ec213ad
Oleg <97077423+RobotSail@users.noreply.github.com>
2022-09-27 09:52:23 -0400

    add multiple e2e tests
    Signed-off-by: Oleg <97077423+RobotSail@users.noreply.github.com>

--- test-kuttl/e2e/ipfs/15-test-data-creation.sh
+++ test-kuttl/e2e/ipfs/15-test-data-creation.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
-
 set -e -o pipefail
 
-########################################
-# logs a message to stdout.
-# Globals:
-#		None.
-# Arguments:
-# 	A list of strings to print out.
-########################################
-log() {
-	input="${*}"
-	printf "[%s]: %s" $(date) "${input}"
-}
-
+source './utils.sh'
 
 # get the name of an IPFS Cluster pod
 crdName='ipfs-sample-1'
@@ -26,9 +14,8 @@ log "writing a file to ${ipfsClusterPodName}"
 kubectl exec -n "${NAMESPACE}" "${ipfsClusterPodName}" -c ipfs-cluster -- sh -c 'echo "hello from ${HOSTNAME} at $(date)" > /tmp/testfile.txt'
 myCID=$(kubectl exec -n "${NAMESPACE}" "${ipfsClusterPodName}" -c ipfs-cluster -- sh -c 'ipfs-cluster-ctl add /tmp/testfile.txt' | awk '{print $2}')
 
-# get the value from the other ipfs node
+# read the value
 ipfsClusterPodname2=$(kubectl get pod -n "${NAMESPACE}" -l "${labelName}=${labelValue}" -o jsonpath='{.items[1].metadata.name}')
 log "reading a file from ${ipfsClusterPodname2}"
-ipfsCommand="ipfs get ${myCID}" 
+ipfsCommand="ipfs get --output /tmp/myfile.txt -- ${myCID}" 
 kubectl exec -n "${NAMESPACE}" "${ipfsClusterPodname2}" -c ipfs -- sh -c "${ipfsCommand}"
-
---
+++ test-kuttl/e2e/ipfs/utils.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e -o pipefail
+
+
+########################################
+# logs a message to stdout.
+# Globals:
+#		None.
+# Arguments:
+# 	A list of strings to print out.
+########################################
+log() {
+	input="${*}"
+	printf "[%s]: %s" $(date) "${input}\n"
+}

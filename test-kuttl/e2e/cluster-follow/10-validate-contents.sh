e2f3c13b26d535f51499c39327df5ab74590186b
Oleg <97077423+RobotSail@users.noreply.github.com>
2022-10-07 14:51:42 -0400

    validate pin test
    Signed-off-by: Oleg <97077423+RobotSail@users.noreply.github.com>

--- test-kuttl/e2e/cluster-follow/10-validate-contents.sh
+++ test-kuttl/e2e/cluster-follow/10-validate-contents.sh
@@ -1,27 +1,59 @@
 #!/bin/bash
-current_shell() {
-  currentShell=$(readlink /proc/"${$}"/exe)
-  printf "using shell: %s\n" "${currentShell}"
-  version=$("${currentShell}" --version)
-  printf "shell version: %s\n" "${version}"
-}
-current_shell
-
-# show all available shells
-cat /etc/shells
-
 set -eo pipefail
       
 # imports
 source './utils.sh'
-      
-log "verifying contents"
-      
-# get the name of an IPFS Cluster pod
-crdName='cluster-1'
-labelValue="ipfs-cluster-${crdName}"
-labelName='app.kubernetes.io/name'
-ipfsClusterPodName=$(kubectl get pod -n "${NAMESPACE}" -l "${labelName}=${labelValue}" -o jsonpath='{.items[0].metadata.name}')
-      
-# ensure that the corresponding IPFS container has pinned content
-pins=$(kubectl exec "${ipfsClusterPodName}" -n "${NAMESPACE}" -c 'ipfs' -- ipfs)
+
+########################################
+# Count the amount of pins in the IPFS node
+# Arguments:
+#   Name of IPFS pod
+# 	Name of follow container
+# Globals:
+#   NAMESPACE 
+# Returns:
+#   Number of pinned nodes
+########################################
+count_pins() {
+	declare podName="${1}"
+	declare followContainerName="${2}"
+	declare -i numPins=0
+	declare pinType=""
+	declare pinTypeRecursive="recursive"
+
+	# ensure that the corresponding IPFS container has pinned content
+  pins=$(kubectl exec "${podName}" -n "${NAMESPACE}" -c "${followContainerName}" -- ipfs pin ls | grep -i "${pinTypeRecursive}")
+  IFS=$'\n'
+  for pin in "${pins}"; do
+		numPins=$((numPins + 1))
+	done
+	echo "${numPins}"
+}
+
+main() {
+  log "verifying contents"
+
+	# default number of pins
+	declare -i DEFAULT_PINS=2
+	
+  # get the name of an IPFS Cluster pod
+  declare crdName='cluster-1'
+  declare labelValue="ipfs-cluster-${crdName}"
+  declare labelName='app.kubernetes.io/name'
+  declare ipfsClusterPodName=$(kubectl get pod -n "${NAMESPACE}" -l "${labelName}=${labelValue}" -o jsonpath='{.items[0].metadata.name}')
+
+	# get number of pins:
+	declare -i numPins=0
+	while [[ "${numPins}" -le "${DEFAULT_PINS}" ]]; do
+		numPins=$(count_pins "${ipfsClusterPodName}")
+		log "current number of pins: ${numPins}"
+		if [[ "${numPins}" -gt "${DEFAULT_PINS}" ]]; then 
+			break
+		else
+			log "not enough pins, sleeping..."
+			sleep 5
+		fi
+	done
+}
+
+main

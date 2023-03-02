98cf06ec5eb5c12be57807718a954b1fbfa35140
Oleg S <97077423+RobotSail@users.noreply.github.com>
2023-03-02 13:52:16 -0500

    add extra log messages to find failpoint
    Signed-off-by: Oleg S <97077423+RobotSail@users.noreply.github.com>

--- test-kuttl/e2e-copy/private-network/10-test-data-creation.sh
+++ test-kuttl/e2e-copy/private-network/10-test-data-creation.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 
+
 # imports
+echo "sourcing utils.sh"
 source './utils.sh'
 
 
 
 main() {
+  echo "this is the first message once the script begins to run"
   # get the name of an IPFS Cluster pod
   crdName='private-ipfs'
   labelValue="ipfs-cluster-${crdName}"
@@ -14,15 +17,19 @@ main() {
   ipfsClusterPodName=$(kubectl get pod -n "${NAMESPACE}" -l "${labelName}=${labelValue}" -o jsonpath='{.items[0].metadata.name}')
   
   # write a file to the ipfs-cluster container in the pod
-  log "writing a file to ${ipfsClusterPodName}"
+  echo "writing a file to ${ipfsClusterPodName}"
   kubectl exec -n "${NAMESPACE}" "${ipfsClusterPodName}" -c ipfs-cluster -- sh -c 'echo "hello from ${HOSTNAME} at $(date)" > /tmp/testfile.txt'
+  echo "wrote the file, grabbing the content ID"
   myCID=$(kubectl exec -n "${NAMESPACE}" "${ipfsClusterPodName}" -c ipfs-cluster -- sh -c 'ipfs-cluster-ctl add /tmp/testfile.txt' | awk '{print $2}')
+  echo "content ID is: ${myCID}"
   
   # read the value
+  echo "getting the other ipfs cluster podname"
   ipfsClusterPodname2=$(kubectl get pod -n "${NAMESPACE}" -l "${labelName}=${labelValue}" -o jsonpath='{.items[1].metadata.name}')
-  log "reading a file from ${ipfsClusterPodname2}"
   ipfsCommand="ipfs get --output /tmp/myfile.txt -- ${myCID}" 
+  echo "reading a file from ${ipfsClusterPodname2} using command: '${ipfsCommand}'"
   kubectl exec -n "${NAMESPACE}" "${ipfsClusterPodname2}" -c ipfs -- sh -c "${ipfsCommand}"
+  echo "success!"
 }
 
 main

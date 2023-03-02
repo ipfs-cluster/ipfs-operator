b5bc3b14c8fa93a7a770e45a0c50469f33c833f9
Oleg S <97077423+RobotSail@users.noreply.github.com>
2023-03-02 13:11:10 -0500

    temporarily remove ipfs directly
    Signed-off-by: Oleg S <97077423+RobotSail@users.noreply.github.com>

---
+++ test-kuttl/e2e-copy/private-network/10-test-data-creation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eo pipefail
+
+# imports
+source './utils.sh'
+
+
+
+main() {
+  # get the name of an IPFS Cluster pod
+  crdName='private-ipfs'
+  labelValue="ipfs-cluster-${crdName}"
+  labelName='app.kubernetes.io/name'
+  ipfsClusterPodName=$(kubectl get pod -n "${NAMESPACE}" -l "${labelName}=${labelValue}" -o jsonpath='{.items[0].metadata.name}')
+  
+  # write a file to the ipfs-cluster container in the pod
+  log "writing a file to ${ipfsClusterPodName}"
+  kubectl exec -n "${NAMESPACE}" "${ipfsClusterPodName}" -c ipfs-cluster -- sh -c 'echo "hello from ${HOSTNAME} at $(date)" > /tmp/testfile.txt'
+  myCID=$(kubectl exec -n "${NAMESPACE}" "${ipfsClusterPodName}" -c ipfs-cluster -- sh -c 'ipfs-cluster-ctl add /tmp/testfile.txt' | awk '{print $2}')
+  
+  # read the value
+  ipfsClusterPodname2=$(kubectl get pod -n "${NAMESPACE}" -l "${labelName}=${labelValue}" -o jsonpath='{.items[1].metadata.name}')
+  log "reading a file from ${ipfsClusterPodname2}"
+  ipfsCommand="ipfs get --output /tmp/myfile.txt -- ${myCID}" 
+  kubectl exec -n "${NAMESPACE}" "${ipfsClusterPodname2}" -c ipfs -- sh -c "${ipfsCommand}"
+}
+
+main

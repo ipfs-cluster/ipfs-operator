a7bf4de551e93172221f185fca275136536f74ba
Oleg S <97077423+RobotSail@users.noreply.github.com>
2023-03-02 15:28:51 -0500

    check information when running test
    Signed-off-by: Oleg S <97077423+RobotSail@users.noreply.github.com>

--- test-kuttl/e2e-copy/private-network/10-test-data-creation.sh
+++ test-kuttl/e2e-copy/private-network/10-test-data-creation.sh
@@ -8,6 +8,7 @@ source './utils.sh'
 
 
 echo "checking to see if awk is installed: '$(which awk)'"
+echo "checking if grep is installed: '$(which grep)'"
 
 main() {
   echo "this is the first message once the script begins to run"
@@ -20,6 +21,16 @@ main() {
   # write a file to the ipfs-cluster container in the pod
   echo "writing a file to ${ipfsClusterPodName}"
   local results=$(kubectl exec -n "${NAMESPACE}" "${ipfsClusterPodName}" -c ipfs-cluster -- sh -c 'echo "hello from ${HOSTNAME} at $(date)" > /tmp/testfile.txt')
+
+  echo "list out the contents of temp, check for testfile"
+  results=$(kubectl exec -n "${NAMESPACE}" "${ipfsClusterPodName}" -c ipfs-cluster -- sh -c 'ls -al /tmp | grep testfile')
+  echo "results: '${results}'"
+  echo "contents of new file:"
+  local contents=$(kubectl exec -n "${NAMESPACE}" "${ipfsClusterPodName}" -c ipfs-cluster -- sh -c 'cat /tmp/testfile.txt')
+  echo "file contents: '${contents}'"
+  echo "checking for ipfs-cluster-ctl"
+  local ipfsClusterCMD=$(kubectl exec -n "${NAMESPACE}" "${ipfsClusterPodName}" -c ipfs-cluster -- sh -c 'which ipfs-cluster-ctl')
+  echo "ipfs-cluster-ctl: $(ipfsClusterCMD)"
 
   # this fails
   echo "grabbing the content ID"
